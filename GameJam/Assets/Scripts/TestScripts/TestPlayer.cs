cf697b5fe2302a06d41fc473419b0fed6c9a8363
Ethan Heil <eheil20@gmail.com>
2020-03-28 15:36:11 -0400

    Adding basic test code for networking

---
+++ GameJam/Assets/Scripts/TestScripts/TestPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class TestPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private Vector3 movement = new Vector3();
+
+    [Client]
+    private void Update()
+    {
+        if (!hasAuthority) { return; }
+
+        if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+
+        //transform.Translate(movement);
+
+        CmdMove();
+    }
+
+    [Command]
+    private void CmdMove()
+    {
+        //Validate logic here
+
+        RpcMove();
+    }
+
+    [ClientRpc]
+    private void RpcMove()
+    {
+        transform.Translate(movement);
+    }
+}
