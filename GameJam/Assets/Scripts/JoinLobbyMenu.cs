ec2b16342c6cf1f5c5e5c84cbb981c8c80fe0053
Ethan Heil <eheil20@gmail.com>
2020-03-28 17:31:50 -0400

    Starting work on lobby system for multiplayer

---
+++ GameJam/Assets/Scripts/JoinLobbyMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerParkour networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        NetworkManagerParkour.OnClientConnected += HandleClientConnected;
+        NetworkManagerParkour.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManagerParkour.OnClientConnected -= HandleClientConnected;
+        NetworkManagerParkour.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        landingPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
---
+++ GameJam/Assets/Scripts/PlayerNameInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+
+    public static string DisplayName { get; private set; }
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpInputField();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+        nameInputField.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    void SetPlayerName(string name)
+    {
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    void SavePlayerName()
+    {
+        DisplayName = nameInputField.text;
+        PlayerPrefs.SetString(PlayerPrefsNameKey, DisplayName);
+    }
+}
