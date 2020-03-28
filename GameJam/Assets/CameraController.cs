3568789044e45c0303bc69cab7034e7abff9fb62
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2020-03-28 14:45:46 -0400

    Adding Base Movement Code

---
+++ GameJam/Assets/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 _mouseAbsolute;
+    Vector2 _smoothMouse;
+
+    public GameObject characterBody;
+
+    [SerializeField]
+    private Vector2 clampInDegrees = new Vector2(360, 180);
+    [SerializeField]
+    private Vector2 sensitivity = new Vector2(2, 2);
+    [SerializeField]
+    private Vector2 smoothing = new Vector2(3, 3);
+    [SerializeField]
+    private Vector2 targetDirection;
+    [SerializeField]
+    private Vector2 targetCharacterDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetDirection = transform.localRotation.eulerAngles;
+
+        if (characterBody)
+            targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        var targetOrientation = Quaternion.Euler(targetDirection);
+        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
+
+
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+
+        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+
+        _mouseAbsolute += _smoothMouse;
+
+        if (clampInDegrees.x < 360)
+            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+
+        if (clampInDegrees.y < 360)
+            _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+
+        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
+
+        if (characterBody)
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+            characterBody.transform.localRotation = yRotation * targetCharacterOrientation;
+        }
+        else
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+            transform.localRotation *= yRotation;
+        }
+    }
+}
--- GameJam/Assets/Movement.cs
+++ GameJam/Assets/Movement.cs
@@ -10,15 +10,42 @@ public class Movement : MonoBehaviour
     public float speed;
     public float rotationSpeed;
 
+    private float xAxis;
+    private float zAxis;
+
+    private Vector3 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Sprint();
+        zAxis = Input.GetAxisRaw("Vertical");
+        xAxis = Input.GetAxisRaw("Horizontal");
+        moveDirection = (xAxis * transform.right + zAxis * transform.forward).normalized;
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection * speed * Time.deltaTime;
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            speed *= 2f;
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+            speed /= 2f;
+    }
+
+    private void Jump()
+    {
+
+    }
+
 }
